d798734e23c8748ea53f42ff25ab772be9189021
i50685837-sketch <i50685837@gmail.com>
2026-08-11 07:59:32 +0300

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+
+function auth(req, res, next) {
+  const header = req.headers.authorization;
+
+  if (!header || !header.startsWith("Bearer ")) {
+    return res.status(401).json({
+      message: "Authentication required"
+    });
+  }
+
+  const token = header.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(
+      token,
+      process.env.AUTH_SECRET
+    );
+
+    req.user = decoded;
+    next();
+
+  } catch {
+    res.status(401).json({
+      message: "Invalid or expired token"
+    });
+  }
+}
+
+module.exports = auth;
